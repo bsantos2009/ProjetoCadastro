afc5b0d358001cb14e57dfefba2a2c520cc71a69
Bruno <bsantosp2009@gmail.com>
2025-10-28 21:06:24 -0300

    4-Criando as classes Usuario e produto na Models

---
+++ ProjetoCadastro/Models/Produto.cs
@@ -0,0 +1,11 @@
+﻿namespace ProjetoCadastro.Models
+{
+    public class Produto
+    {
+        public int Id { get; set; }
+        public required string Nome { get; set; }
+        public string? Descricao { get; set; }
+        public decimal Preco { get; set; }
+        public decimal quantidade { get; set; }
+    }
+}
---
+++ ProjetoCadastro/Models/Usuario.cs
@@ -0,0 +1,10 @@
+﻿namespace ProjetoCadastro.Models
+{
+    public class Usuario
+    {
+        public int Id { get; set; }
+        public required string Nome { get; set; }
+        public required string Email { get; set; }
+        public required string Senha { get; set; }
+    }
+}
